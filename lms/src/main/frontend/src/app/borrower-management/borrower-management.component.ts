1a7ba26becda5215628d4165d7dacddf0d13242e
Srikumar Ramaswamy <srikumar4794@gmail.com>
2020-05-30 23:41:51 -0500

    Borrower form.

--- lms/src/main/frontend/src/app/borrower-management/borrower-management.component.ts
+++ lms/src/main/frontend/src/app/borrower-management/borrower-management.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-borrower-management',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BorrowerManagementComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
   }
 
+  borrowerForm: FormGroup;
+
+  ngOnInit(): void {
+    this.initializeBorrowerForm();
+  }
+
+  initializeBorrowerForm() {
+    this.borrowerForm = this.fb.group(
+      {
+        name: new FormGroup({
+          firstName: new FormControl("", Validators.required),
+          lastName: new FormControl("", Validators.required)
+        }),
+        address: new FormGroup({
+          street: new FormControl(""),
+          apt: new FormControl(""),
+          city: new FormControl(""),
+          state: new FormControl(""),
+          zipCode: new FormControl("")
+        }),
+        ssn: new FormControl("", Validators.required),
+        phoneNum: new FormControl("")
+      }
+    );
+  }
 }
