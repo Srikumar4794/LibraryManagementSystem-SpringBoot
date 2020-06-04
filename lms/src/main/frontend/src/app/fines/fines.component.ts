f29220f894fc45cb65c157139501fd9890726207
Srikumar Ramaswamy <srikumar4794@gmail.com>
2020-06-04 01:14:36 -0500

    Total fine calculation

--- lms/src/main/frontend/src/app/fines/fines.component.ts
+++ lms/src/main/frontend/src/app/fines/fines.component.ts
@@ -11,14 +11,15 @@ export class FinesComponent implements OnInit {
   selectedCardID: number;
   displayCardIdHead: number;
   columns: any = [
-               {field: 'loanId', header: 'Loan ID', width:'15px'},
-               {field: 'cardId', header: 'Card ID', width:'15px'},
+               {field: 'loanId', header: 'Loan ID', width:'10px'},
+               {field: 'cardId', header: 'Card ID', width:'10px'},
                {field: 'isbn', header: 'ISBN', width:'30px'},
-               {field: 'bookName', header: 'Book Name', width:'50px'},
+               {field: 'bookName', header: 'Book Name', width:'80px'},
                {field: 'fineAmount', header: 'Fine', width:'15px'},
                {field: 'paid', header: 'Fine Status', width:'30px'},
               ];
   fineData: Fine[];
+  totalFine: number = 0;
 
   constructor(private fineService: FineService) { }
 
@@ -35,6 +36,7 @@ export class FinesComponent implements OnInit {
     this.fineService.getFinesForBorrower(this.selectedCardID).subscribe(
       (data) => {
         this.fineData = data;
+        this.totalFine = this.fineData.map(fine => fine.fineAmount).reduce((a, b) => a + b);
         this.displayCardIdHead = this.selectedCardID;
       }
     )
